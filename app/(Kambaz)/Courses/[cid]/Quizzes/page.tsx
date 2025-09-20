150e517b74da824d3d83be82303ed5bf5a0471ea
VOIDED <bmackmrhs@gmail.com>
2025-09-19 23:25:43 -0400

    Added starter pages to avoid 404 errors

---
+++ app/(Kambaz)/Courses/[cid]/Quizzes/page.tsx
@@ -0,0 +1,8 @@
+export default function QuizzesPage() {
+  return (
+    <div id="wd-quizzes">
+      <h2>Quizzes</h2>
+      <p>This is a starter page for the Quizzes screen.</p>
+    </div>
+  );
+}
---
+++ app/(Kambaz)/Courses/[cid]/Zoom/page.tsx
@@ -0,0 +1,8 @@
+export default function ZoomPage() {
+  return (
+    <div id="wd-zoom">
+      <h2>Zoom</h2>
+      <p>This is a starter page for the Zoom screen.</p>
+    </div>
+  );
+}
